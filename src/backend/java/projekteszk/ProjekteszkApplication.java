c33a05aceed766b732cebeee693175fcc358483a
Mézes Ádám <mezesadam1997@gmail.com>
2019-03-11 18:41:11 +0100

    add alap fájlok

---
+++ src/backend/java/projekteszk/ProjekteszkApplication.java
@@ -0,0 +1,13 @@
+package projekteszk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekteszkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekteszkApplication.class, args);
+	}
+
+}
