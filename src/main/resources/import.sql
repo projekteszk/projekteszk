bef3bb52fb5cafd727d954ddebf1c13e08fdaf92
Mézes Ádám <mezesadam1997@gmail.com>
2019-03-13 14:52:08 +0100

    add product tábla insert teszt adatok

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `PRODUCT` (`NAME`, `TYPE`, `MANUFACTURER`, `PRICE`, `DESC`) VALUES ('Teszt termek', 'Teszt', 'Teszt gyarto', 1000, 'Valami');
+INSERT INTO `PRODUCT` (`NAME`, `TYPE`, `MANUFACTURER`, `PRICE`, `DESC`) VALUES ('Teszt termek 2', 'Teszt', 'Teszt gyarto', 1500, 'Valami');
+INSERT INTO `PRODUCT` (`NAME`, `TYPE`, `MANUFACTURER`, `PRICE`, `DESC`) VALUES ('Teszt termek 3', 'Teszt', 'Teszt gyarto', 2000, 'Valami');
