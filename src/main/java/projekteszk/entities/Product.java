9b650a6182271e3da897ae891553cb7524fb760f
Mézes Ádám <mezesadam1997@gmail.com>
2019-03-17 08:05:26 +0100

    change Product entity, notNull import kivevése

--- src/main/java/projekteszk/entities/Product.java
+++ src/main/java/projekteszk/entities/Product.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
