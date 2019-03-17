51303f68c7756036e3e6ca3c304683bc5914396c
palakos <paldi.akos@gmail.com>
2019-03-17 11:45:53 +0100

    add Mohosz entity, repo, controller

---
+++ src/main/java/projekteszk/controllers/MohoszController.java
@@ -0,0 +1,36 @@
+package projekteszk.controllers;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import projekteszk.entities.Mohosz;
+import projekteszk.repositories.MohoszRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/mohosz")
+public class MohoszController {
+    @Autowired
+    private MohoszRepository mohoszRepository;
+    
+    @GetMapping("")
+    public ResponseEntity<Iterable<Mohosz>> getAll() {
+        return ResponseEntity.ok(mohoszRepository.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Mohosz> get(@PathVariable Integer id) {
+        Optional<Mohosz> oMohosz = mohoszRepository.findById(id);
+        if (!oMohosz.isPresent()) {
+            return ResponseEntity.notFound().build();   
+        }
+        
+        return ResponseEntity.ok(oMohosz.get());
+    }
+    
+}
---
+++ src/main/java/projekteszk/entities/Mohosz.java
@@ -0,0 +1,49 @@
+package projekteszk.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Mohosz implements Serializable {
+    
+    @Id
+    private Integer card_id;
+         
+    /*
+    @JoinColumn
+    @ManyToOne
+    private Club club;
+    */
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date card_issue;
+    
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date card_expiration;
+            
+    @Column
+    private Integer fishing_license_numb;
+    
+    @Column
+    private Boolean is_disabled;
+    
+    
+}
